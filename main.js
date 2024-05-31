a570271f795c4c4ec3b2a76b06c0d7fa5061f758
Evan Pun <121113256+epun21403@users.noreply.github.com>
2024-05-31 14:30:57 -0400

    modified assessment

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Global currentCatTax variable
 let currentCatTax = 0;
-
+const amountOwed = document.getElementById("amountOwed");
+const payTaxButton = document.getElementById("payButton");
+const catImgContainer = document.getElementById("catContainer");
+const page = document.getElementById("wholeContainer");
 // TODO: calcButtonClick function
 // Function should handle the following items:
 // 1) Generate a random, whole number between 0 and 20.
@@ -11,7 +14,22 @@ let currentCatTax = 0;
 // 6) If the random number is 0, update the pay button so that it is hidden.
 // 7) Both the amountOwed and pay amount button should be updated every time the calculate cat tax button is clicked.
 
-function calcButtonClick() {}
+function calcButtonClick() {
+  currentCatTax = Math.floor(Math.random() * 21);
+  amountOwed.innerHTML = "You owe " + currentCatTax + " cat tax! Pay up!"
+  if (currentCatTax === 0) {
+    amountOwed.innerHTML = "You owe " + currentCatTax + " cat tax! You've escaped this time!"
+  }
+  if (currentCatTax !== 0) {
+    payTaxButton.innerHTML = "Pay Cat Tax"
+    payTaxButton.style.opacity = 100;
+  }
+
+  if (currentCatTax === 0) {
+    payTaxButton.style.opacity = 0;
+  }
+
+}
 
 // TODO: payButton function
 // Function should handle the following items:
@@ -22,4 +40,25 @@ function calcButtonClick() {}
 // 5) Once the cat image is retrieved, append the image to the image container
 // 6) If the cat wax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
 
-function payButton() {}
+async function payButton() {
+  if (currentCatTax > 0) {
+    currentCatTax--;
+    amountOwed.innerHTML = "You owe " + currentCatTax + " cat tax! Pay up!"
+
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await response.json();
+
+    const catImageUrl = (data[0].url)
+    const catImageHolder = document.createElement("img");
+    catImageHolder.src = catImageUrl;
+    catImgContainer.append(catImageHolder);
+  }
+
+  if (currentCatTax <= 0) {
+    amountOwed.innerHTML = "Your debts are paid...";
+    const paidCatImage = document.createElement("img");
+    paidCatImage.src = "https://gfycat.com/snivelingbeautifuljoey-cat";
+    page.innerHTML = "";
+    page.append(paidCatImage);
+  }
+}
